8c650b541bbc18d369cb384d0913dc3c792a7dc1
ShadowMole <steven.bruman@yahoo.com>
2019-04-24 14:43:11 -0400

    Broken file pathing

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy
+import os
 
+for i in range(4):
+    imname = '\\..\\..\\src\\TrainingImages\\Steven'+str(i+1)+'.jpg'
+    path = os.getcwd() + imname
+    print(path)
+    image = cv2.imread(imname,0)
+    cv2.imshow('hi',image)
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    vector = []
+    for i in range(gray.shape[0]):
+        for j in range(gray.shape[1]):
+            vector.append(gray[i,j])
